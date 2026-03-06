ad8c6e194e084bb9dab6300a1804ca5273a3d461
rakibulhasan02 <rakibul.ict02@gmail.com>
2026-03-07 02:16:04 +0600

    design cashout form and logical implementation

---
+++ scripts/cashout.js
@@ -0,0 +1,11 @@
+console.log('hi cashout');
+document.getElementById('cashout-btn').addEventListener('click',function(){
+    //   1- get the agent number and validate
+    // 2-get the amount ,validate , convert to number
+    // 3-get the current balance , validate,convert to number
+    // 4-calculate new balance
+    // 5-get the pin and verify
+    //5-1 true:::> show an alert > set balence
+    // 5-2 false:: > show an alert > return
+
+});
