c62d751fa760c938315d1f904a240def7e2a9cb0
rakibulhasan02 <rakibul.ict02@gmail.com>
2026-03-07 00:47:12 +0600

    try to fix the issue. the issue is do not show other page

--- scripts/login.js
+++ scripts/login.js
@@ -21,7 +21,7 @@ document.getElementById('login-btn').addEventListener('click',function(){
         alert('Login Successfully');
         //add home page
         // window.location.replace("/home.html");
-        window.location.assign("/home.html");
+        window.location.assign("home.html");
     }
     else{
         // 3-2 false:::> alert and return
